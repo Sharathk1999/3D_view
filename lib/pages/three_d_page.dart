f8d730709b845e494d4219cc1150382ad59223a3
Sharath Kumar <1999sharathkumar7@gmail.com>
2023-11-30 18:42:25 +0530

    added left,right,center and 3D view

--- lib/pages/three_d_page.dart
+++ lib/pages/three_d_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:o3d/o3d.dart';
+
 class ThreeDPage extends StatefulWidget {
   const ThreeDPage({super.key});
 
@@ -8,22 +9,101 @@ class ThreeDPage extends StatefulWidget {
 }
 
 class _ThreeDPageState extends State<ThreeDPage> {
+  O3DController controller = O3DController();
+  bool isEnabled = false;
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         elevation: 0.0,
-        
-         title:const Row(
+        title: const Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('3D'),
-            Text('Model',style: TextStyle(color: Colors.amberAccent),),
+            Text(
+              'Model',
+              style: TextStyle(color: Colors.amberAccent),
+            ),
           ],
-         ),
+        ),
       ),
-      body: O3D(
-        src: "assets/avatar/6568600be0834c9ab3853159.glb",
+      body: Stack(
+        alignment: Alignment.bottomRight,
+        children: [
+          O3D(
+            controller: controller,
+            cameraControls: false,
+            autoPlay: true,
+            src: "assets/avatar/6568600be0834c9ab3853159.glb",
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 10, right: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  FloatingActionButton(
+                    backgroundColor: Colors.amberAccent.withOpacity(0.6),
+                    onPressed: () {
+                      controller.cameraOrbit(0, 75, 20);
+                      controller.cameraTarget(0, 0.9, -2.3);
+                    },
+                    child: const Icon(Icons.zoom_in_rounded),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  FloatingActionButton(
+                    backgroundColor: Colors.amberAccent.withOpacity(0.6),
+                    onPressed: () {
+                      controller.cameraOrbit(90, 75, 20);
+                      controller.cameraTarget(1, 1, 0);
+                    },
+                    child: const Icon(Icons.swipe_left_rounded),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  FloatingActionButton(
+                    backgroundColor: Colors.amberAccent.withOpacity(0.6),
+                    onPressed: () {
+                      controller.cameraOrbit(0, 75, 20);
+                      controller.cameraTarget(0, 0.9, 0);
+                    },
+                    child: const Icon(Icons.center_focus_weak_rounded),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  FloatingActionButton(
+                    backgroundColor: Colors.amberAccent.withOpacity(0.6),
+                    onPressed: () {
+                      controller.cameraOrbit(-90, 75, 20);
+                      controller.cameraTarget(-1, 1, 0);
+                    },
+                    child: const Icon(Icons.swipe_right_rounded),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  FloatingActionButton(
+                    backgroundColor: Colors.amberAccent.withOpacity(0.6),
+                    onPressed: () {
+                      controller.cameraControls = true;
+                    },
+                    child: const Icon(Icons.threed_rotation_rounded,
+                        color: Colors.red),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
