f062cf40b9fe180c2720a926515c966c22167a8a
Sharath Kumar <1999sharathkumar7@gmail.com>
2023-11-30 16:55:41 +0530

    displayed 3D avatar

--- lib/pages/three_d_page.dart
+++ lib/pages/three_d_page.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
-
-class ThreeDPage extends StatelessWidget {
+import 'package:o3d/o3d.dart';
+class ThreeDPage extends StatefulWidget {
   const ThreeDPage({super.key});
 
   @override
+  State<ThreeDPage> createState() => _ThreeDPageState();
+}
+
+class _ThreeDPageState extends State<ThreeDPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(),
+    return  Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        
+         title:const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('3D'),
+            Text('Model',style: TextStyle(color: Colors.amberAccent),),
+          ],
+         ),
+      ),
+      body: O3D(
+        src: "assets/avatar/6568600be0834c9ab3853159.glb",
+      ),
     );
   }
 }
